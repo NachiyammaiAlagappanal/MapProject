1a5245afb5a24f4471586a04c2d38a882a485f72
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-06-24 11:51:24 +0530

    2d Array format.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { React } from 'react';
 import './App.scss';
+import GetCommon from './components/GetCommon.js';
 
 const App = () =>
 	<div className="App" role="App">
-		Ready to start.
+		{ GetCommon() }
 	</div>;
 
 export default App;
--- src/components/GetCommon.js
+++ src/components/GetCommon.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
-// import { map } from '@laufire/utils/collection';
-
+// import { unique } from '@laufire/utils/predicates';
 const Data = [
 	[{ name: 'apple', quantity: 10 },
 		{ name: 'orange', quantity: 15 },
@@ -13,14 +12,22 @@ const Data = [
 	[{ name: 'apple', quantity: 10 },
 		{ name: 'Papaya', quantity: 40 }],
 ];
+const GetCommon = () => {
+	const GetLength = (data) => data.map((ele) => ele.length);
 
-const GetLength = (data) => data.map((ele) => ele.length);
+	const MinValue = Math.min(...GetLength(Data));
 
-const MinValue = Math.min(...GetLength(Data));
+	const index = GetLength(Data).indexOf(MinValue);
 
-const index = GetLength(Data).indexOf(MinValue);
+	const MinimumLengthArray = Data[index];
+	const Ele = MinimumLengthArray.map((ele) => ele.name);
 
-const MinimumLengthArray = Data[index];
-const Ele = MinimumLengthArray.map((ele) => ele.name);
+	const Result = Data.map((element) => Ele.map((e) =>
+		element.find((t) => t.name === e)));
 
-console.log(Ele);
+	console.log(Result);
+
+	return Result;
+};
+
+export default GetCommon;
